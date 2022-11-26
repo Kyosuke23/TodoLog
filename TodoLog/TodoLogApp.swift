1f295d74f07ab26f3cd1a2e4e354c9b8bfda1722
Kyosuke <love.frisk1188@gmail.com>
2022-11-26 14:09:31 +0900

    Initial Commit

---
+++ TodoLog/TodoLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoLogApp.swift
+//  TodoLog
+//
+//  Created by 池田匡佑 on 2022/11/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
